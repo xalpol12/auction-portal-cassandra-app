610c34448f91d1a9636596f8b68c9d8956a62777
xalpol12 <xalpol12@gmail.com>
2024-10-23 13:46:09 +0200

    init

---
+++ src/main/java/com/xalpol12/thesis_alien_game/ThesisAlienGameApplication.java
@@ -0,0 +1,13 @@
+package com.xalpol12.thesis_alien_game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisAlienGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThesisAlienGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xalpol12/thesis_alien_game/ThesisAlienGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xalpol12.thesis_alien_game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisAlienGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
