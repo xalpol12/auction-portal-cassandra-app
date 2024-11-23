484cdd409eb090ac173f09a0acf8188e0bf949b8
xalpol12 <xalpol12@gmail.com>
2024-11-23 19:57:09 +0100

    Move default local calues to application.properties, create tables

--- src/main/java/com/xalpol12/auctionportal/dbconnector/CassandraConnector.java
+++ src/main/java/com/xalpol12/auctionportal/dbconnector/CassandraConnector.java
@@ -6,14 +6,18 @@ import com.datastax.driver.core.Session;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class CassandraConnector {
 
+    @Value("${CASS_HOST_NAME}")
+    private String hostName;
     @Value("${REPLICATION_STRATEGY}")
     private String replicationStrategy;
     @Value("${REPLICATION_FACTOR}")
@@ -23,16 +27,6 @@ public class CassandraConnector {
     private Session session;
     private Cluster cluster;
 
-    public CassandraConnector() {
-        connect();
-    }
-
-    public void connect() {
-        Builder builder = Cluster.builder().addContactPoint("cass-1").withPort(9042);
-        cluster = builder.build();
-        session = cluster.connect();
-    }
-
     @PreDestroy
     public void close() {
         session.close();
@@ -40,8 +34,29 @@ public class CassandraConnector {
     }
 
     @PostConstruct
-    public void setUpDb() {
-        if (initializeKeyspace()) log.info("Keyspace initialised successfully") else ;
+    public void init() {
+        connect();
+        setUpDb();
+    }
+
+    private void connect() {
+        Builder builder = Cluster.builder().addContactPoint(hostName).withPort(9042);
+        cluster = builder.build();
+        session = cluster.connect();
+    }
+
+    private void setUpDb() {
+        if (initializeKeyspace()) {
+            log.info("Keyspace initialised successfully");
+        } else {
+            log.info("Keyspace not initialised");
+        }
+
+        if (initializeTables()) {
+            log.info("Tables initialised successfully");
+        } else {
+            log.info("Tables not initialised");
+        }
     }
 
     private boolean initializeKeyspace() {
@@ -56,11 +71,45 @@ public class CassandraConnector {
 
     private boolean initializeTables() {
         StringBuilder sb = new StringBuilder("CREATE TABLE IF NOT EXISTS ")
-                .append("").append("(")
-                .append("subject text);");
+                .append("USERS").append("(")
+                .append("id UUID, ")
+                .append("name UUID, ")
+                .append("auctions LIST<UUID>, ")
+                .append("PRIMARY KEY (name));");
 
-        String query = sb.toString();
-        var result = session.execute(query);
-        return result.wasApplied();
+        String createUsers = sb.toString();
+        var usersResult = session.execute(createUsers);
+
+        StringBuilder sb2 = new StringBuilder("CREATE TABLE IF NOT EXISTS ")
+                .append("AUCTIONS").append("(")
+                .append("id UUID, ")
+                .append("start_date TIMESTAMP, ")
+                .append("end_date TIMESTAMP, ")
+                .append("auction_name text, ")
+                .append("start_price DECIMAL, ")
+                .append("status text, ")
+                .append("auction_winner UUID, ")
+                .append("PRIMARY KEY (status, id, end_date))")
+                .append("WITH CLUSTERING ORDER BY (id ASC, end_date ASC);");
+        String createAuctions = sb2.toString();
+        var auctionsResult = session.execute(createAuctions);
+
+        // TODO: Ponder idea of creating redundancy in tables to get easier access to data
+
+        StringBuilder sb3 = new StringBuilder("CREATE TABLE IF NOT EXISTS ")
+                .append("BIDS").append("(")
+                .append("auction_id UUID, ")
+                .append("id UUID, ")
+                .append("user_id UUID, ")
+                .append("bid_value DECIMAL, ")
+                .append("bid_timestamp TIMESTAMP, ")
+                .append("bid_validity TEXT, ")
+                .append("PRIMARY KEY (auction_id, bid_value))")
+                .append("WITH CLUSTERING ORDER BY (bid_value DESC);");
+
+        String createBids = sb3.toString();
+        var bidsResult = session.execute(createBids);
+
+        return usersResult.wasApplied() && auctionsResult.wasApplied() && bidsResult.wasApplied();
     }
 }
